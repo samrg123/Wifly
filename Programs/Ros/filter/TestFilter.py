6d7d69f0130b6c0fcd7f72751dfb56e5cf35e87a
burner <burner@burners-MacBook-Pro.local>
2022-04-04 21:34:45 -0400

    Added PF updates for prediction/correction

--- Programs/Ros/filter/TestFilter.py
+++ Programs/Ros/filter/TestFilter.py
@@ -16,6 +16,18 @@ class TestFilter:
 
         self.state = RobotState()
 
+        self.Q = system.Q # Measuerment Covariance
+        self.LQ = np.linalg.cholesky(self.Q)
+        self.R = system.R # Wifi Noise
+        self.noisy = system.noisy # Flag to determine if noise is added
+
+        self.n = init.n
+        w = 1/self.n
+        # self.p = system.p
+        self.p_w = w*np.zeros(init.n).reshape(init.n, 1)
+        L = np.linalg.cholesky(init.Sigma)
+        self.p = L@np.random.randn(size(init.Sigma)[0], init.n) + init.mu
+
         # init state
         self.state.SetMean(init.mu)
         self.state.SetCovariance(init.Sigma)
@@ -29,15 +41,50 @@ class TestFilter:
         #       ACCELERATION MODEL AND INTRODUCES DRIFT!
 
         # simply propagate the state and assign identity to covariance
-        predictedState = self.motionFunction(state, sensorValue, deltaT)
+        for i in self.p.size()[1]: 
+            if self.noisy: 
+                state = self.p[:, i] + self.LQ@np.random.randn(len(self.p[:, 0]), 1)
+            else: 
+                state = self.p[:, i]
+            self.p[:, i] = self.motionFunction(state, sensorValue, deltaT)
         predictedCovariance = covariance
 
-        self.state.SetMean(predictedState.GetMean())
+        self.state.SetMean(np.mean(self.p))
+        # self.state.SetMean(predictedState.GetMean())
         self.state.SetCovariance(predictedCovariance)
 
+    def intensity_query_client(pos): 
+        rospy.wait_for_service("intensity")
+        try: 
+            intensity_serv = rospy.ServiceProxy("intensity", intensity)
+            intensity_val = intensity_serv(pos)
+            return intensity_val
+        except rospy.ServiceException as e: 
+            print(f"Exception {e} occurred")
 
     def correction(self, z):
-        assert False, "Not Implemented"
+
+        w = np.zeros((self.n, 1))
+        
+        for i in self.p.size()[1]: 
+            v = z - intensity_query_client(self.p[:, i])
+            w[i] = multivariate_normal.pdf(v.reshape(-1), np.zeros(len(self.p.size()[0])), self.R)
+
+        self.p_w = np.multiply(self.p_w, w)
+        self.p_w = self.p_w/np.sum(self.p_w)
+        self.Neff = 1/np.sum(np.power(self.p_w), 2)
+
+    def resampling(self): 
+        W = np.cumsum(self.p_w)
+        r = rand(1) / self.n
+        j = 1
+        for i in range(self.n): 
+            u = r + (i-1)/self.n
+            while u > W[j]: 
+                j = j + 1
+            self.p[:, i] = self.p[:, j]
+            self.p_w[i] = 1 / self.n
+
 
     def GetState(self):
         return deepcopy(self.state)
