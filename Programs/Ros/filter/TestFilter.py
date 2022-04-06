4bd609198c1390f457b24d7f5e56041381ec822b
wilcohe <willcohe@umich.edu>
2022-04-05 22:33:11 -0400

    Added gen_pt call

--- Programs/Ros/filter/TestFilter.py
+++ Programs/Ros/filter/TestFilter.py
@@ -82,7 +82,7 @@ class TestFilter:
         w = np.zeros((self.n, 1))
         for i in range(len(self.p)):
             pos = self.p[i].GetPosition()
-            v = z - intensity_query_client(pos)
+            v = z - intensity_query_client(gen_pt(pos))
             w[i] = multivariate_normal.pdf(v.reshape(-1), np.zeros(len(self.p.shape[0])), self.R)
 
         self.p_w = np.multiply(self.p_w, w)
