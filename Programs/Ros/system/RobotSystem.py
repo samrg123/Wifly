dca485b25c458d9c5b1c418eca6a0d7742688076
wilcohe <willcohe@umich.edu>
2022-04-05 22:45:36 -0400

    Testing...

--- Programs/Ros/system/RobotSystem.py
+++ Programs/Ros/system/RobotSystem.py
@@ -72,7 +72,7 @@ class RobotSystem:
             # update model
             print(sample.groundTruthState.GetPosition())
             self.filter.prediction(sample.sensorValue, sample.deltaT)
-            self.filter.correction(intensity_query_client(gen_pt(sample.groundTruthState.GetPosition())).intensity)
+            # self.filter.correction(intensity_query_client(gen_pt(sample.groundTruthState.GetPosition())).intensity)
             
             # print("N_PRED:", self.filter.GetState())
             # print("CMD:   ", sample.commandState)
